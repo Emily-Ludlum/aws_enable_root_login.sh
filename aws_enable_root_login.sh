d963fd1c988b209e5f5803413a74fa5cc07798c1
Emily-Ludlum <160514488+Emily-Ludlum@users.noreply.github.com>
2024-03-21 07:12:05 -0500

    aws_enable_root_login.sh

--- aws_enable_root_login.sh
+++ aws_enable_root_login.sh
@@ -18,4 +18,17 @@ sed -i 's/^UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
 echo "Editing authorized_keys file..."
 sed -i 's/ssh-rsa .*$/ssh-rsa/g' /root/.ssh/authorized_keys
 
+# 5. 重启 SSH 服务
+echo "Restarting SSH service..."
+if [ -x "$(command -v systemctl)" ]; then
+    systemctl restart sshd
+elif [ -x "$(command -v service)" ]; then
+    service ssh restart
+else
+    echo "Unable to restart SSH service. Please restart it manually."
+fi
+
+echo "SSH service restarted."
+
+
 echo "Script execution completed."
