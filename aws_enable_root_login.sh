da7f7fa5c185948d2d1ee84225014f80ba05fe58
Emily-Ludlum <160514488+Emily-Ludlum@users.noreply.github.com>
2024-03-22 03:13:44 -0500

    aws_enable_root_login.sh

--- aws_enable_root_login.sh
+++ aws_enable_root_login.sh
@@ -10,8 +10,8 @@ su - root -c "echo 'Switched to root user.'"
 
 # 第三步：编辑ssh配置文件
 echo "Editing sshd_config file..."
-sudo sed -i 's/#PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/#\?PasswordAuthentication\s\+no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/#\?PermitRootLogin\s\+prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sudo sed -i 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
 
 # 第四步：编辑authorized_keys文件
