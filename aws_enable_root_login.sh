3d1c879b89aa00201e344c0d87a57c8a48ca7a5a
Emily-Ludlum <160514488+Emily-Ludlum@users.noreply.github.com>
2024-03-22 02:25:58 -0500

    aws_enable_root_login.sh

--- aws_enable_root_login.sh
+++ aws_enable_root_login.sh
@@ -1,33 +1,18 @@
 #!/bin/bash
 
-# 1. 创建root用户及设置密码
-echo "Setting password for root user..."
-echo "root:Abcd@1234" | chpasswd
+# 第一步：创建root用户并设置密码
+echo -e "Abcd@1234\nAbcd@1234" | sudo passwd root
 
-# 2. 切换到root用户
-echo "Switching to root user..."
-su - root -c "echo 'Switched to root user'"
+# 第二步：切换到root用户
+sudo su -
 
-# 3. 修改SSH配置文件
-echo "Editing SSH configuration..."
-sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sed -i 's/^#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config
-sed -i 's/^#UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
+# 第三步：编辑SSH配置文件
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/#PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/#UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
 
-# 4. 编辑authorized_keys文件
-echo "Editing authorized_keys file..."
-sed -i 's/#.*ssh-rsa/ssh-rsa/g' /root/.ssh/authorized_keys
+# 第四步：修改authorized_keys文件
+sudo sed -i 's/ssh-rsa.*$/ssh-rsa/g' /root/.ssh/authorized_keys
 
-# 5. 重启 SSH 服务
-echo "Restarting SSH service..."
-if [ -x "$(command -v systemctl)" ]; then
-    systemctl restart sshd
-elif [ -x "$(command -v service)" ]; then
-    service ssh restart
-else
-    echo "Unable to restart SSH service. Please restart it manually."
-fi
-
-echo "SSH service restarted."
-
-echo "Script execution completed."
+# 重启SSH服务
+sudo systemctl restart sshd
