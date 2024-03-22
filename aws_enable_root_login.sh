207e7d07ad386c38ce132fc9346a578e2799cc37
Emily-Ludlum <160514488+Emily-Ludlum@users.noreply.github.com>
2024-03-22 03:49:58 -0500

    aws_enable_root_login.sh

--- aws_enable_root_login.sh
+++ aws_enable_root_login.sh
@@ -10,12 +10,12 @@ su - root -c "echo 'Switched to root user.'"
 
 # 第三步：编辑ssh配置文件
 echo "Editing sshd_config file..."
-sudo sed -i 's/#\?PasswordAuthentication\s\+no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo sed -i 's/#\?PermitRootLogin\s\+prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
-sudo sed -i 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
+sudo vim -c ":%s/#\?PasswordAuthentication\s\+no/PasswordAuthentication yes/g" -c ":wq" /etc/ssh/sshd_config
+sudo vim -c ":%s/#\?PermitRootLogin\s\+prohibit-password/PermitRootLogin yes/g" -c ":wq" /etc/ssh/sshd_config
+sudo vim -c ":%s/UsePAM yes/UsePAM no/g" -c ":wq" /etc/ssh/sshd_config
 
 # 第四步：编辑authorized_keys文件
 echo "Editing authorized_keys file..."
-sudo sed -i 's/^.*ssh-rsa/ssh-rsa/g' /root/.ssh/authorized_keys
+sudo vim -c ":%s/^.*ssh-rsa/ssh-rsa/g" -c ":wq" /root/.ssh/authorized_keys
 
 echo "All steps completed."
